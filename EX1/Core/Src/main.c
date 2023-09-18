fb8749abcee527a97f57b2633d26444910d63fc4
NQHuy14 <95513347+NQHuy14@users.noreply.github.com>
2023-09-18 13:26:27 +0700

    nextChange5

--- EX1/Core/Src/main.c
+++ EX1/Core/Src/main.c
@@ -62,9 +62,6 @@ static void MX_GPIO_Init(void);
   * @brief  The application entry point.
   * @retval int
   */
-typedef enum{
-	RED_NS,YELLOW_NS,GREEN_NS
-}direction;
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -95,48 +92,11 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  int delay=0;
-  direction NS=RED_NS;
   while (1)
   {
-	  switch(NS){
-  case RED_NS:
-	  HAL_GPIO_WritePin(RED_NS_GPIO_Port, RED_NS_Pin, RESET);
-	  HAL_GPIO_WritePin(YELLOW_NS_GPIO_Port, YELLOW_NS_Pin, SET);
-	  HAL_GPIO_WritePin(GREEN_NS_GPIO_Port, GREEN_NS_Pin, SET);
+    /* USER CODE END WHILE */
 
-	  HAL_GPIO_WritePin(RED_WE_GPIO_Port, RED_WE_Pin, SET);
-	  HAL_GPIO_WritePin(YELLOW_WE_GPIO_Port, YELLOW_WE_Pin, SET);
-	  HAL_GPIO_WritePin(GREEN_WE_GPIO_Port, GREEN_WE_Pin, RESET);
-	  NS=GREEN_NS;
-	  delay=5000;
-	  break;
-  case GREEN_NS:
-	  HAL_GPIO_WritePin(RED_NS_GPIO_Port, RED_NS_Pin, SET);
-	  HAL_GPIO_WritePin(YELLOW_NS_GPIO_Port, YELLOW_NS_Pin, SET);
-	  HAL_GPIO_WritePin(GREEN_NS_GPIO_Port, GREEN_NS_Pin, RESET);
-
-	  HAL_GPIO_WritePin(RED_WE_GPIO_Port, RED_WE_Pin, RESET);
-	  HAL_GPIO_WritePin(YELLOW_WE_GPIO_Port, YELLOW_WE_Pin, SET);
-	  HAL_GPIO_WritePin(GREEN_WE_GPIO_Port, GREEN_WE_Pin, SET);
-	  NS=YELLOW_NS;
-	  delay=3000;
-	  break;
-  case YELLOW_NS:
-	  HAL_GPIO_WritePin(RED_NS_GPIO_Port, RED_NS_Pin, SET);
-	  HAL_GPIO_WritePin(YELLOW_NS_GPIO_Port, YELLOW_NS_Pin, RESET);
-	  HAL_GPIO_WritePin(GREEN_NS_GPIO_Port, GREEN_NS_Pin, SET);
-
-	  HAL_GPIO_WritePin(RED_WE_GPIO_Port, RED_WE_Pin, SET);
-	  HAL_GPIO_WritePin(YELLOW_WE_GPIO_Port, YELLOW_WE_Pin, RESET);
-	  HAL_GPIO_WritePin(GREEN_WE_GPIO_Port, GREEN_WE_Pin, SET);
-	  delay=2000;
-	  NS=RED_NS;
-	  break;
-
-
-  }
-  HAL_Delay(delay);
+    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
